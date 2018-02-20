778ab4b35895bdbe1564a42940afb88d93612c6f
Pitágoras Alves <alves.pitagoras@gmail.com>
2018-02-19 22:19:26 -0300

    Package installation script

---
+++ installPackage.R
@@ -0,0 +1,21 @@
+installFromPeridotRepo <- function(packName, repo){
+  install.packages(packName, dependencies=TRUE, repos=repo)
+}
+
+packIsInstalled <- function(s){
+  return(s %in% rownames(installed.packages()))
+}
+
+packageToInstall <- args[length(args)-1]
+repo <- args[length(args)]
+
+if(packIsInstalled(packageToInstall)){
+  "Package already installed. Skiping installation."
+}else{
+  installFromPeridotRepo(packageToInstall, repo)
+  if(packIsInstalled(packageToInstall)){
+    "Package successfully installed."
+  }else{
+    "The package could not be installed successfully."
+  }
+}
