21a56ce4ff196a25ee7f1b77feb98f94af1d2a7a
Pitágoras Alves <alves.pitagoras@gmail.com>
2018-03-18 00:54:03 -0300

    After failing to install packages from R-Peridot repo, trying from CRAN (R-Cloud by R-Project) and then Bioconductor.

--- installPackage.R
+++ installPackage.R
@@ -1,3 +1,13 @@
+installFromBioLite <- function(packName){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite()
+  biocLite(packName)
+}
+
+installFromCran <- function(packName){
+  install.packages(packName, dependencies=TRUE, repos="https://cloud.r-project.org/")
+}
+
 installFromPeridotRepo <- function(packName, repo){
   install.packages(packName, dependencies=TRUE, repos=repo)
 }
@@ -6,16 +16,26 @@ packIsInstalled <- function(s){
   return(s %in% rownames(installed.packages()))
 }
 
+args = commandArgs(trailingOnly = F)
 packageToInstall <- args[length(args)-1]
 repo <- args[length(args)]
 
+installFromPeridotRepo(packageToInstall, repo)
+
 if(packIsInstalled(packageToInstall)){
-  "Package already installed. Skiping installation."
+  "Package successfully installed."
 }else{
-  installFromPeridotRepo(packageToInstall, repo)
+  "Could not install from R-Peridot repositories, trying to download from a CRAN mirror."
+  installFromCran(packageToInstall)
   if(packIsInstalled(packageToInstall)){
     "Package successfully installed."
   }else{
-    "The package could not be installed successfully."
+    "Could not install from CRAN repository, trying to download from Bioconductor."
+    installFromBioLite(packageToInstall)
+    if(packIsInstalled(packageToInstall)){
+      "Package successfully installed."
+    }else{
+      "The package could not be installed successfully."
+    }
   }
 }
