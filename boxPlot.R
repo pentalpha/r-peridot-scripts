0c4181b8396e14cf7e7e0093985b9397a295340f
Pitágoras Alves <alves.pitagoras@gmail.com>
2018-09-12 16:19:17 -0300

    Initial boxplot: 	Displaying condition colors 	More space to display sample names 	Blue -> Yellow color pallete

--- boxPlot.R
+++ boxPlot.R
@@ -17,26 +17,11 @@ options(bitmapType='cairo')
 #Read file example
 pasillaCountTable = read.table(countReads, header=TRUE, row.names=1 )
 
-geneNames = rownames(pasillaCountTable)
+sampleNames = rownames(pasillaCountTable)
 
-head(geneNames)
+head(sampleNames)
 
 pasillaDesign = read.table(conditions, header=TRUE, row.names=1)
-pasillaDesign
-
-#Ignore samples with "not-use" indicated
-#first, remove they from the conditions table
-pasillaDesign <- subset(pasillaDesign, condition != "not-use")
-#then, remove from the counts table
-for(i in colnames(pasillaCountTable)){
-  iContainsNotUse = length(grep("not.use", as.name(i))) > 0
-  if(iContainsNotUse){
-    #erases the column
-    pasillaCountTable[, i] = NULL
-  }
-}
-#Finally, drop unused levels (not-use levels)
-pasillaDesign = droplevels(pasillaDesign)
 pasillaDesign
 
 #Normalization
@@ -44,16 +29,65 @@ pasillaCountTable = as.data.frame(lapply(pasillaCountTable, function(x) (x/sum(x
 
 names = colnames(pasillaCountTable)
 
-row.names(pasillaCountTable) = geneNames
+row.names(pasillaCountTable) = sampleNames
 
 head(pasillaCountTable)
 
 lev = levels(pasillaDesign$condition)
 
-color.code<-colorRampPalette(c('blue','red'), space="rgb")(length(lev))
+color.code<-colorRampPalette(c('blue','yellow'), space="rgb")(length(lev))
 
-png(filename = saveTo, width=600, height=600)
+conditionColor<-function(condition, conditions, cols){
+  finalColor <- cols[1]
+  for (i in 1:length(conditions)){
+    if (condition == conditions[i]){
+      finalColor <- cols[i]
+    }
+  }
+  finalColor
+}
+colors <- color.code[pasillaDesign$condition]
+colors
+levelColors = c()
+levelIndex = 1
+for(cond in lev){
+  color <- conditionColor(cond, pasillaDesign$condition, colors)
+  levelColors <- c(levelColors, color)
+  print(color)
+}
 
-boxplot(pasillaCountTable, outline = F, col = color.code[pasillaDesign$condition])
+levelColors
 
+png(filename = saveTo, width = 600, height = 600)
+par(xpd = T, mar = par()$mar + c(0.5,0,-3,7))
+boxplot(pasillaCountTable, outline = F, col = colors, axes = FALSE, axisnames = FALSE)
+
+getPos<-function(pos, bounds, coord){
+  xmin <- bounds[1]
+  xmax <- bounds[2]
+  ymin <- bounds[3]
+  ymax <- bounds[4]
+  xsize <- xmax - xmin
+  ysize <- ymax - ymin
+
+  position <- xmin + (xsize * pos)
+  if (coord == "y"){
+    position <- ymin + (ysize * pos)
+  }
+  position
+}
+
+text(seq(1, length(pasillaDesign$condition), by=1), getPos(0.027,par("usr"),"y"), 
+  labels = row.names(pasillaDesign), srt = 45, adj = c(1.1,1.1), xpd = TRUE, cex=.9)
+axis(2)
+
+ncols <- ceiling(length(levelColors) / 3)
+ncols
+par("usr")
+x <- getPos(1.013,par("usr"),"x")
+y <- getPos(1.0,par("usr"),"y")
+legend(x, y, 
+  title="Conditions", lev, fill=levelColors, ncol=1,cex = 0.8)
+
+par(mar=c(5, 4, 4, 2) + 0.1)
 dev.off()
